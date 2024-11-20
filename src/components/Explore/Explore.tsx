a74160d71a33de3aa75ed3835a8cec983b6cec1f
IannisG10 <guerraiannis@gmail.com>
2024-11-20 15:19:03 +0100

    expertise

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import webIlustration from "../../assets/image/Illustration web.png";
+import webIlustration from "../../assets/Avatar/Avatar Explore.png"
 import s from "./Explore.module.css"
 
 const Explore: React.FC = () => {
     return(
-        <div className="flex justify-start items-center bg-gray-100">
-            <img src={webIlustration} alt="illustration" className={`${s.exploreContainer} w-[400px]`} />
-            <div className="flex flex-col justify-start gap-7">
+        <div className={`${s.exploreBackGround} relative flex justify-evenly items-center p-10 bg-gray-100`}>
+            <div className="flex flex-col justify-start gap-7 absolute z-10 left-28">
                 <h1 className="font-nunito font-semibold text-5xl w-[550px]">Discover my Services and my Expertise</h1>
                 <p className="font-monsterrat text-zinc-300 w-[650px]">
                         As a developper,I offer tailored solutions to bring your ideas to life.
@@ -17,6 +16,7 @@ const Explore: React.FC = () => {
                         Explore my Services
                 </button>
             </div>
+            <img src={webIlustration} alt="illustration" className={`w-[482px] absolute right-0`} />
         </div>
     )
 }
