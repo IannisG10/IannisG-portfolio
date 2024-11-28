3763033d4809457996b62b82fa2335630fbfb569
IannisG10 <guerraiannis@gmail.com>
2024-11-28 07:10:48 +0100

    change the root of public image

--- src/components/MyProject/MyProject.tsx
+++ src/components/MyProject/MyProject.tsx
@@ -9,7 +9,7 @@ const MyProject: React.FC = () => {
                 {DataProject.map((item,index) => (
                     <div key={index} 
                         className="flex flex-col justify-center gap-3 p-2 mx-3 items-center bg-purple-100 border-gray-50 shadow-md rounded-md">
-                            <img src={`/public/${item.imageProject}.jpg`} alt={item.tittleProject}
+                            <img src={`/${item.imageProject}.jpg`} alt={item.tittleProject}
                                 className="w-[150px] rounded-md cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out"
                             />
                             <h2 className="font-semibold font-monsterrat md:text-2xl text-base">{item.tittleProject}</h2>
