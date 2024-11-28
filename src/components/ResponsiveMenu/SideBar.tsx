a70f40b778dfe7474017a1f6383c9d4dd363183e
IannisG10 <guerraiannis@gmail.com>
2024-11-28 06:44:47 +0100

    fixed the side bar

--- src/components/ResponsiveMenu/SideBar.tsx
+++ src/components/ResponsiveMenu/SideBar.tsx
@@ -9,7 +9,7 @@ interface menuProps{
 
 const SideBar: React.FC<menuProps> = ({openMenu})=>{
     return(
-        <div className={`flex justify-stretch flex-col items-center rounded-sm bg-purple-200 w-1/2 h-1/3 absolute top-12 right-2 ${openMenu ? "block ":"hidden "} `}>   
+        <div className={`flex justify-stretch flex-col items-center rounded-sm bg-purple-200 w-1/2 h-1/3  fixed top-12 right-2 ${openMenu ? "block ":"hidden "} `}>   
             <ul className="flex flex-col justify-center gap-5 items-center text-sm cursor-pointer py-5">
                 <li className={`text-black`}>Home</li>
                 <li className={`text-black`}>About</li>
